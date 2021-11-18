ce1e082ed679f6d342fbbc62dd51ad721122872a
Taneros <fricentaurus@GMAIL.COM>
2021-11-18 19:05:35 +0300

    feat: add active cat active highlight

--- art-quiz/src/components/controller.js
+++ art-quiz/src/components/controller.js
@@ -7,6 +7,10 @@ let activeGameMode
 let menuAuthorQuiz
 let menuPictureQuiz
 
+// categories dropdown
+let catDropdown
+let catDropdownNode
+
 function setActiveGameMode(node) {
   if (activeGameMode) {
     activeGameMode.classList.remove('active')
@@ -17,8 +21,18 @@ function setActiveGameMode(node) {
 
 export default {
   async authorRoute(params) {
-    menuAuthorQuiz = document.querySelector('[data-role="menu-author"]')
     // console.log(params.category)
+    menuAuthorQuiz = document.querySelector('[data-role="menu-author"]')
+    // set active cat
+    catDropdown = document.querySelectorAll('div.main-nav > ul > li.nav-item.dropdown > ul > li')
+    catDropdown.forEach((el, id) => {
+      // console.log(el)
+      if (el.firstChild.id === params.category) {
+        console.log('catdropdown same cat', el.firstChild)
+        catDropdownNode = el.firstChild
+      }
+    })
+
     const authors = Model.getAuthor(params.category)
     console.log(authors)
     Author.setData(authors, params.category)
@@ -28,13 +42,26 @@ export default {
      * specify how many authors to load?
      *
      */
-    setActiveGameMode(menuAuthorQuiz)
+    setActiveGameMode(menuAuthorQuiz, params.category)
+    setActiveGameMode(catDropdownNode)
   },
 
-  async pictureRoute() {
+  async pictureRoute(params) {
+    console.log('picure route')
     menuPictureQuiz = document.querySelector('[data-role="menu-pictures"]')
-    const pictures = Model.getImg()
-    // console.log(pictures)
+    console.log('menuPictureQuiz', menuPictureQuiz)
+    // set active cat
+
+    catDropdown = document.querySelectorAll('div.main-nav > ul > li.nav-item.dropdown > ul > li')
+    catDropdown.forEach((el, id) => {
+      // console.log(el)
+      if (el.firstChild.id === params.category) {
+        console.log('catdropdown same cat', el.firstChild)
+        catDropdownNode = el.firstChild
+      }
+    })
+    const pictures = Model.getImg(params.category)
+    console.log('pictureRoute pictures:', pictures)
     // TODO
     /**
      * specify how many pictures to load ?
@@ -42,6 +69,7 @@ export default {
      */
 
     setActiveGameMode(menuPictureQuiz)
+    setActiveGameMode(catDropdownNode)
   },
   async mainRoute() {},
 }
